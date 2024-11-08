5739815c8bc40d783215db5d5ef5ef706e81133e
Adrian M <adrian.im0502@gmail.com>
2024-11-07 23:36:12 -0600

    Two new operators were implemented, PMX and Swap mutation. The operators will be used to test the operators against native operators in combinatorial problems

--- new_src/evaluation/operators/crossover.py
+++ new_src/evaluation/operators/crossover.py
@@ -30,4 +30,61 @@ class SBX_Cross(Crossover):
         return Xp
     
 
+class PMX_Cross(Crossover):
 
+    def __init__(self, prob, **kwargs):
+        super().__init__(2, 2, prob, **kwargs)
+
+    def is_permutation(self, arr):
+
+        n = len(arr)
+
+        arr_set = set(arr)
+
+        return len(arr_set) == n and min(arr) == 0 and max(arr) == n-1
+
+    def _do(self, problem, X, **kwargs):
+
+        # Input shape (n_parents, n_matings, n_var)
+        _, n_matings, n_var = X.shape
+
+        # Initialize offspring array
+        offspring = np.full((2, n_matings, n_var), -1)
+
+        for i in range(n_matings):
+
+            # Get parents
+            parent1, parent2 = X[0, i], X[1, i]
+
+            # Choose two crossover points
+            cx_point1, cx_point2 = sorted(np.random.choice(n_var, 2, replace=False))
+
+            # Create offspring for current mating by copying crossover segments
+            offspring[0, i, cx_point1:cx_point2] = parent1[cx_point1:cx_point2]
+            offspring[1, i, cx_point1:cx_point2] = parent2[cx_point1:cx_point2]
+
+            # Mapping function
+            def map_genes(o, p, cx1, cx2):
+                for j in range(n_var):
+                    if j < cx1 or j >= cx2:
+                        gene = p[j]
+                        while gene in o[cx1:cx2]:
+                            gene = p[np.where(o==gene)[0][0]]
+                        o[j] = gene
+
+            # Map genes
+            map_genes(offspring[0, i], parent2, cx_point1, cx_point2)
+            map_genes(offspring[1, i], parent1, cx_point1, cx_point2)
+
+            # Verify they are permutation
+            if self.is_permutation(offspring[0, i]) and self.is_permutation(offspring[1, i]):
+                continue
+
+            else:
+                raise(ValueError)
+
+            # Replace -1 values with genes from the other parent
+            #offspring[0, i] = np.where(offspring[0, i] == -1, parent2, offspring[0,i])
+            #offspring[1, i] = np.where(offspring[1, i] == -1, parent1, offspring[1,i])
+
+        return offspring
--- new_src/evaluation/operators/mutation.py
+++ new_src/evaluation/operators/mutation.py
@@ -29,3 +29,34 @@ class PM_Mutation(Mutation):
 
         return Xp
     
+
+class Swap_Mutation(Mutation):
+
+    def __init__(self, prob, **kwargs) -> None:
+        super().__init__(prob, **kwargs)
+
+        self.probability = prob
+
+    def _do(self, problem, X, **kwargs):
+        
+        # X -> Input shape (n_individuals, n_var)
+        n_individuals, n_var = X.shape
+
+        # Create a copy of X to mutate individuals
+        mutated_X = np.copy(X)
+
+        if self.probability > 0:
+
+            # For each individual
+            for i in range(n_individuals):
+
+                # Apply mutation based on the mutation rate
+                if np.random.rand() < self.probability:
+
+                    # Select two positions to swap
+                    pos1, pos2 = np.random.choice(n_var, 2, replace=False)
+
+                    # Swap
+                    mutated_X[i, pos1], mutated_X[i, pos2] = mutated_X[i, pos2], mutated_X[i, pos1]
+
+        return mutated_X
